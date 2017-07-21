e119fbbed9ec4880675d04ecea39618833423249
Andy Nowacki <a.nowacki@leeds.ac.uk>
2017-07-21 07:24:16 +0100

    Add plot_splits_hist for polar histograms
    `plot_splits_hist` and `plot_splits_hist!` by default use a
    different colour for each station, but this can be overridden
    by passing a formatting keyword argument through to Plots.
    
    Also add `bar` to create a bar `Shape` which can be used in future
    to replace the quiver plot for `plot_splits_bar`.

--- src/SWSPlots.jl
+++ src/SWSPlots.jl
@@ -2,24 +2,29 @@
 ## SWSPlots
 
 The SWSPlots module provides plotting functions to show maps with shear wave splitting
-results
+results.
 
 """
 module SWSPlots
 
 using Geodesy, Plots
+
 using SWSs
+using CircPlot
 
 export
     plot_splits_bars,
-    plot_splits_bars!
+    plot_splits_bars!,
+    plot_splits_hist,
+    plot_splits_hist!
 
 const SWSArr = Union{SWS, Array{SWS}}
+const PlotSubplot = Union{Plots.Plot,Plots.Subplot}
 
 """
     plot_splits_bars!(p::Plots.Plot, splits::SWSArr) -> p::Plots.Plot
 """
-function plot_splits_bars!(p::Plots.Plot, splits::SWSArr;
+function plot_splits_bars!(p::PlotSubplot, splits::SWSArr;
         km=false, scale=km?10:1e4, location=:station, kwargs...)
     s = splits
     x, y = if location == :station
@@ -44,5 +49,58 @@ function plot_splits_bars!(p::Plots.Plot, splits::SWSArr;
     p
 end
 plot_splits_bars(splits::SWSArr; kwargs...) = plot_splits_bars!(plot(), splits; kwargs...)
+
+function plot_splits_hist!(p::PlotSubplot, splits::SWSArr;
+        km=false, binwidth=20, maxsize=km?5:5e3, minobs=3, kwargs...)
+    s = splits
+    stas, sx, sy = sta_coords(s)
+    km && (sx /= 1e3; sy /= 1e3)
+    hists = Array{Array{Plots.Shape}}(0)
+    colours = [:red, :green, :blue, :orange, :white]
+    for (i,sta) in enumerate(sort(stas))
+        plot!(p, chistogram(s[s[:stnm] .== sta][:phi], binwidth, true, sx[i], sy[i],
+            axial=true, azimuth=true, maxr=maxsize, minobs=minobs);
+            c=colours[mod1(i, length(colours))], label=sta, kwargs...)
+    end
+    p
+end
+plot_splits_hist(splits::SWSArr; kwargs...) = plot_splits_hist!(plot(), splits; kwargs...)
+
+"""
+    sta_coords(s) -> stas, x, y
+
+Return arrays of station names `stas`, x-coordinates, `x` and y-coordinates `y`.
+"""
+function sta_coords(s::SWSArr)
+    stas = unique(s[:stnm])
+    x, y = Float64[], Float64[]
+    for sta in stas
+        push!(x, s[s[:stnm] .== sta][1].sx)
+        push!(y, s[s[:stnm] .== sta][1].sy)
+    end
+    stas, x, y
+end
+
+"""
+    bar(ϕ, length, x=0, y=0, width=length/20) -> ::Shape
+
+Return a `Plots.Shape` representing a bar oriented parallel to azimuth `ϕ` °
+and `length` plot units long.  `width` specifies the bar width in plot units.
+The bar is centred at `(x, y)`.
+"""
+function bar(ϕ, length, x=0., y=0., width=length/20.)
+    l2 = length/2
+    w2 = width/2
+    ϕ = deg2rad(ϕ)
+    sp, cp = sin(ϕ), cos(ϕ)
+    p1x, p1y = l2*sp, l2*cp
+    shape = Array{Tuple{Float64,Float64}}(4)
+    i = 0
+    for (lpm ,wpm) in zip((1, -1, -1, 1), (1, 1, -1, -1))
+        i += 1
+        shape[i] = (x + lpm*p1x + wpm*w2*sin(ϕ-π/2), y + lpm*p1y + wpm*w2*cos(ϕ-π/2))
+    end
+    Plots.Shape(shape)
+end
 
 end # module
